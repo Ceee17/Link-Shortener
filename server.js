b37c3804eb78c82ecfa5e7558a9c1c64b47190db
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2024-03-02 22:37:05 +0700

    Refactor codebase and move table to the admin section

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-// /Projects/server.js
-
+// server.js
 const express = require("express");
 const mongoose = require("mongoose");
 const ShortUrl = require("./models/shortUrl");
 const app = express();
 
 mongoose.connect("mongodb://localhost/linkShortener", {
-  bufferCommands: false, // Disable command buffering
+  bufferCommands: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+
 app.get("/", async (req, res) => {
   const shortUrls = await ShortUrl.find();
-  res.render("index", { shortUrls: shortUrls });
+  res.render("index", { showShortenedLink: false, shortUrls: shortUrls });
 });
 
-app.post("/shortUrls", async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl });
-  res.redirect("/");
+app.post("/admin/reset", async (req, res) => {
+  await ShortUrl.deleteMany({});
+  res.redirect("/admin");
+});
+
+app.get("/admin", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("admin", { shortUrls: shortUrls });
+});
+app.post("/shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.create({ full: req.body.fullUrl });
+  res.render("index", { showShortenedLink: true, shortUrls: [shortUrl] });
 });
 
 app.get("/:shortUrl", async (req, res) => {
@@ -34,3 +43,5 @@ app.get("/:shortUrl", async (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000);
+
+console.log("App Running at http://localhost:5000");
