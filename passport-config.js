f902ae9822a0d9d6ea91221dbcbd5bb320c1c97d
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2024-05-02 01:44:11 +0700

    fix google callback for prod (2)

--- passport-config.js
+++ passport-config.js
@@ -29,7 +29,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.NODE_ENV !== "production" ? "http://localhost:5000/auth/google/callback" : "http://3.25.64.62/auth/google/callback",
+      callbackURL: process.env.NODE_ENV !== "production" ? "http://localhost:5000/auth/google/callback" : "http://3.25.64.62:5000/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       try {
